9f67af17ef776292cdff9e7e1204bca4b1f3b12d
karthikraja34 <rajakarthik131@gmail.com>
2016-10-04 15:29:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("seerver is started");
+});
