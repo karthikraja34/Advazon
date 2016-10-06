0df6fd6694408cd6f832401c146303b7c8f9e176
karthikraja34 <rajakarthik131@gmail.com>
2016-10-06 12:31:46 +0000

    Login not working

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-var express     = require("express"),
-    app         = express(),
-    passport    = require("passport"),
-    Service     = require("./models/bookings"),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose")
+var express        = require("express"),
+    app            = express(),
+    passport       = require("passport"),
+    LocalStratergy  = require("passport-local").Strategy,
+    Service        = require("./models/bookings"),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose"),
+    User           = require("./models/user");
 
+mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost/adv");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
+
+//  Passport configuration
+app.use(require("express-session")({
+    secret : "advazon key for encription",
+    resave :false,
+    saveUninitialized :false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratergy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 // Service.create({
@@ -32,9 +48,6 @@ app.get("/",function(req,res){
     
 });
 
-app.get("/user",function(req,res){
-    res.render("login");
-});
 
 app.get("/service/:id",function(req, res) {
     var serv = req.params.id;
@@ -62,10 +75,31 @@ app.get("/works",function(req,res){
     res.render("how-it-works");
 });
 
+
+
+// ==========
+// AUTH ROUTES
+// ==========
+app.get("/register",function(req,res){
+    res.render("login");
+});
+app.post("/register",function(req,res){
+   var newUser = new User({username: req.body.name , email : req.body.email , phone : req.body.phone});
+   User.register(newUser,req.body.password,function(err,user){
+       if(err){
+           console.log(err);
+           return res.render("login");
+       }
+       passport.authenticate("local")(req,res,function(){
+           res.redirect("/");
+       });
+       
+   });
+});
+
 app.get("*",function(req, res) {
     res.send("404 page not found");
-})
-
+});
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("seerver is started");
 });
