64cd2d1e307e29bbfb78d88a7013f69bfc493195
karthikraja34 <rajakarthik131@gmail.com>
2016-10-06 02:43:37 +0000

    using cloud9

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
     res.render("home");
